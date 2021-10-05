fe85ae1e27ffcbf42c2aa18c042e5288e6f27219
Omar <omaar_5@hotmail.com>
2021-10-05 14:09:30 +0200

    feat: wrap children in ThemeProvider

--- global/Layout/Layout.js
+++ global/Layout/Layout.js
@@ -1,12 +1,21 @@
+import { ThemeProvider } from "styled-components";
 import { Navbar } from "../../components";
+import { darkTheme, GlobalStyle, lightTheme, typoTheme } from "..";
+import { useSelector } from "react-redux";
+import { selectDarkValue } from "../../redux/slices/darkSlice";
 
 export const Layout = ({ children }) => {
+    const darkValue = useSelector(selectDarkValue);
+
     return (
-        <div>
-            <Navbar />
-            {children}
-            {/* Contact */}
-            {/* Footer */}
-        </div>
+        <ThemeProvider theme={typoTheme}>
+            <ThemeProvider theme={darkValue === "light" ? lightTheme : darkTheme}>
+                <GlobalStyle />
+                <Navbar />
+                {children}
+                {/* Contact */}
+                {/* Footer */}
+            </ThemeProvider>
+        </ThemeProvider>
     );
 };
