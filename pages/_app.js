0fbecc8e4ed2a783b7bd937089d6437bd4b665c4
Omar <omaar_5@hotmail.com>
2021-10-05 13:39:00 +0200

    feat: wrap app in provider and persistGate

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { Layout } from "../global";
+import { persistor, store } from "../redux/store";
 
 const App = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </PersistGate>
+        </Provider>
     );
 };
 export default App;
