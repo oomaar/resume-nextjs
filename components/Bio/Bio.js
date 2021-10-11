fce2d1adfbaaf7fe12ea9a6cae0c72603f7054a7
Omar <omaar_5@hotmail.com>
2021-10-11 16:00:35 +0200

    style: Bio

--- components/Bio/Bio.js
+++ components/Bio/Bio.js
@@ -1,11 +1,36 @@
+import Image from "next/image";
 import {
     BioContainer,
+    BioSubContainer,
+    Title,
+    ImageContainer,
+    BioStory,
+    ResumeButton,
 } from "./styledBio";
 
 export const Bio = () => {
     return (
         <BioContainer>
-            Bio Component
+            <Title>Bio</Title>
+
+            <BioSubContainer>
+                <ImageContainer>
+                    <Image
+                        src="/about.jpg"
+                        width={400}
+                        height={400}
+                        objectFit="contain"
+                    />
+                </ImageContainer>
+                <BioStory>
+                    I am a creative Frontend Engineer and a Self-Learner.
+                    I have always been passionate about everything related to technology and programming,
+                    especially the web professions, which I made my career by always giving the best I can give as a Web Developer.
+                    I also love to code from scratch, and I enjoy bringing ideas to life in the browser.
+                    Aside code I love travelling and making new friends, I love life and people.
+                </BioStory>
+            </BioSubContainer>
+            <ResumeButton>Download Resume</ResumeButton>
         </BioContainer>
     );
 };
--- components/Bio/styledBio.js
+++ components/Bio/styledBio.js
@@ -1,3 +1,67 @@
 import styled from 'styled-components';
+import { lgScreen } from '../../global';
 
-export const BioContainer = styled.div``;
+export const BioContainer = styled.div`
+  color: ${({ theme }) => theme.textColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.titleColor};
+  font-size: ${({ theme }) => theme.fontSize.bigFontSize};
+  font-family: 'Shadows Into Light', cursive;
+`;
+
+export const BioSubContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  @media screen and (max-width: ${lgScreen}) {
+    flex-direction: column;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  width: 50%;
+
+  img {
+    border-radius: 1rem;
+  }
+
+  @media screen and (max-width: ${lgScreen}) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+export const BioStory = styled.p`
+  width: 50%;
+  color: ${({ theme }) => theme.textColor};
+  font-size: ${({ theme }) => theme.fontSize.normalFontSize};
+
+  @media screen and (max-width: ${lgScreen}) {
+    width: 100%;
+  }
+`;
+
+export const ResumeButton = styled.a`
+  background: ${({ theme }) => theme.buttonColor};
+  color: ${({ theme }) => theme.buttonText};
+  font-size: ${({ theme }) => theme.fontSize.linkFontSize};
+  border-radius: 1rem;
+  padding: 0.5rem 2rem;
+  outline: 0;
+  border: 0;
+  margin-top: 2rem;
+
+  :hover {
+    background: ${({ theme }) => theme.buttonHover};
+  }
+`;
